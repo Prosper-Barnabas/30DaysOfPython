f392eaea59f4a8b96e3d1771c0e35ffaab5a7b33
Prosper Barnabas <barnabasprosper.7@gmail.com>
2026-07-12 23:35:41 +0100

    day_2

---
+++ day_2/variables.py
@@ -0,0 +1,52 @@
+# Day 2: 30 Days of python programming
+# Exercises: Level 1
+
+first_name = input('Input your first name: ')
+last_name = input('Input your last name: ')
+full_name = 'Prosper Barnabas'
+country = input('Input your country: ')
+city = 'Port Harcourt'
+age = input('Input your age: ')
+year = 2026
+is_married = False
+is_true = False
+is_light_on = True
+
+# Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(first_name) > len(last_name))
+
+num_one, num_two = 5, 4
+total = num_two + num_one
+print(total)
+diff = num_one - num_two
+print(diff)
+variable = num_two * num_one
+print(variable)
+division = num_one / num_two
+print(division)
+remainder = num_two % num_one
+print(remainder)
+exp = num_one ** num_two
+print(exp)
+floor = num_one // num_two
+print(floor)
+
+PI = 3.14
+radius = input("Input the radius")
+area_of_circle = PI * (int(radius) ** 2)
+circum_of_circle = 2 * PI * 30
+print(area_of_circle)
+print(circum_of_circle)
+
