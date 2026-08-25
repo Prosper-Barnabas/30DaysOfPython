f3f7bc57a3fbef3a9153db366350d50f71c71dc5
Prosper Barnabas <barnabasprosper.7@gmail.com>
2026-08-25 17:25:56 +0100

    day_17

---
+++ day_17/day_exception_handling.py
@@ -0,0 +1,9 @@
+# Exercise: Day 17
+# 1
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+
+print(nordic_countries)
+print(es)
+print(ru)
