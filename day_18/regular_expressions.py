323eb97f10e8d38a9a900363c20defdbd501fc0d
Prosper Barnabas <barnabasprosper.7@gmail.com>
2026-08-27 15:14:09 +0100

    added Exercise: level 2 solution

--- day_18/regular_expressions.py
+++ day_18/regular_expressions.py
@@ -25,7 +25,16 @@ print(distance)
 
 # Exercises: Level 2
 # 1
+def is_valid_variable(text):
+    regex = r'^[A-Za-z_][A-Za-z0-9_]*$'
+    if re.match(regex, text):
+        return True
+    return False
 
+print(is_valid_variable('first_name')) # True
+print(is_valid_variable('first-name')) # False
+print(is_valid_variable('1first_name')) # False
+print(is_valid_variable('firstname')) # True
 
 # Exercises: Level 3
 # 1
