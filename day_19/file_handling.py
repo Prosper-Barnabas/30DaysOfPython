fefc111d5f7881a754c2247de0b493de832b5d3b
Prosper Barnabas <barnabasprosper.7@gmail.com>
2026-09-11 17:53:46 +0100

    solution to problem 2 - Exercise Level 1

--- day_19/file_handling.py
+++ day_19/file_handling.py
@@ -11,7 +11,7 @@ def read_file(filename):
         lines = f.readlines()
 
         words = sum(len(line.split()) for line in lines)
-        return f"The file contains {words} words and {len(lines)} lines"
+        return f"File contains {words} words and {len(lines)} lines"
 
 print(read_file('../data/obama_speech.txt'))
 print(read_file('../data/michelle_obama_speech.txt'))
@@ -19,3 +19,16 @@ print(read_file('../data/donald_speech.txt'))
 print(read_file('../data/melina_trump_speech.txt'))
 
 # 2
+def most_spoken_languages(filename, limit=10):
+    with open(filename, 'r', encoding='utf-8') as file:
+        countries_data = json.load(file)
+        language_dict = {}
+
+        for country in countries_data:
+            for language in country.get('languages', []):
+                language_dict[language] = language_dict.get(language, 0) + 1
+
+        sorted_languages = sorted([(count, name) for name, count in language_dict.items()], reverse=True)[:limit]
+        return sorted_languages
+
+print(most_spoken_languages(filename='../data/countries_data.json', limit=3))
